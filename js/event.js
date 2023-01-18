3aa0d8210d59c7f4e802a0e546986eedf191a94d
John Paul Leyble <jpleyble06@gmail.com>
2023-01-18 13:43:08 +0800

    Changed 18/01/2023 1:43pm

--- js/event.js
+++ js/event.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slidefx");
-  let dots = document.getElementsByClassName("dot");
+  let dots = document.getElementsByClassName("subT");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
